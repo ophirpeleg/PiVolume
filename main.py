ab7a328965c8573a95007b66b76df9c226d6da40
Ophir Peleg <Ophir.peleg1@Gmail.com>
2022-07-08 19:57:15 +0300

    Adding volume function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import board
 import digitalio
+import os
+import os.path
 
 dirPin = digitalio.DigitalInOut(board.D17)
 stepPin = digitalio.DigitalInOut(board.D18)
 dirPin.direction = digitalio.Direction.INPUT
 stepPin.direction = digitalio.Direction.INPUT
+volumeInterval = 50
 
 dirPin.pull = digitalio.Pull.UP
 stepPin.pull = digitalio.Pull.UP
@@ -14,9 +17,11 @@ while 1 == 1:
     if previousValue != stepPin.value:
         if stepPin.value == False:
             if dirPin.value == False:
-                print("Left")
-            else:
                 print("Right")
+                os.system(f"amixer -c 0 set Headphone {volumeInterval}+")
+            else:
+                print("Left")
+                os.system(f"amixer -c 0 set Headphone {volumeInterval}-")
         previousValue = stepPin.value
 
 
