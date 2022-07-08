a6b6a3df61edf18e5bc08763c8588c79f9d6283e
Ophir Peleg <Ophir.peleg1@Gmail.com>
2022-07-08 21:44:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import os.path
 
 dirPin = digitalio.DigitalInOut(board.D2)
-pushPin = digitalio.DigitalInOut(board.D16)
+pushPin = digitalio.DigitalInOut(board.D8)
 stepPin = digitalio.DigitalInOut(board.D3)
 
 dirPin.direction = digitalio.Direction.INPUT
@@ -13,7 +13,7 @@ volumeInterval = 50
 
 dirPin.pull = digitalio.Pull.UP
 stepPin.pull = digitalio.Pull.UP
-# pushPin.pull = digitalio.Pull.UP
+pushPin.pull = digitalio.Pull.UP
 previousValue = True
 
 print("Welcome")
@@ -22,13 +22,11 @@ print(f"push pin value: {pushPin.value}")
 is_on = True
 
 while is_on:
-    if pushPin.value == True:
+    if pushPin.value == False:
         print("False")
 
     if previousValue != stepPin.value:
-        print("Welcome2")
         if stepPin.value == False:
-            print("Welcome3")
             if dirPin.value == False:
                 print("Right")
                 os.system(f"amixer -c 0 set Headphone {volumeInterval}+")
