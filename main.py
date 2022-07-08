9838778cd028d3b73565da514c5c8c9fece435d7
Ophir Peleg <Ophir.peleg1@Gmail.com>
2022-07-08 19:24:31 +0300

    testing1

---
+++ main.py
@@ -0,0 +1,23 @@
+import board
+import digitalio
+
+dirPin = digitalio.DigitalInOut(board.GP17)
+stepPin = digitalio.DigitalInOut(board.GP18)
+dirPin.direction = digitalio.Durection.INPUT
+stepPin.direction = digitalio.Durection.INPUT
+
+dirPin.pull = digitalio.Pull.UP
+stepPin.pull = digitalio.Pull.UP
+previousValue = True
+
+while 1 == 1:
+    if previousValue != stepPin.value:
+        if stepPin.value ==False:
+            if dirPin.value == False:
+                print("Left")
+            else:
+                print("Right")
+        previousValue = stepPin.value
+
+
+
